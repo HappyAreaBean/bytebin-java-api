1d7c1e46f203480c8dc437d686fe7be2a314d7a1
HappyAreaBean <bean@happyareabean.cc>
2024-07-25 18:35:24 +0800

    build: add test

---
+++ src/test/java/PasteFactoryYAMLTest.java
@@ -0,0 +1,37 @@
+import org.imanity.paste.PasteException;
+import org.imanity.paste.PasteFactory;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class PasteFactoryYAMLTest {
+
+    private static final String SERVER_URL = "https://bytebin.happyareabean.cc";
+    private static final String TEST_FILE_PATH = "src/test/resources/test.yml";
+    private static String testContent;
+
+    @BeforeClass
+    public static void setup() throws IOException {
+        testContent = new String(Files.readAllBytes(Paths.get(TEST_FILE_PATH)), StandardCharsets.UTF_8);
+    }
+
+    @Test
+    public void testWriteAndFind() throws PasteException {
+        PasteFactory pasteFactory = PasteFactory.create(SERVER_URL);
+
+        String key = pasteFactory.write(testContent, "text/yaml");
+        System.out.println(SERVER_URL + "/" + key);
+        assertNotNull(key);
+
+        String content = pasteFactory.find(key);
+        System.out.println(content);
+        assertEquals(testContent, content);
+    }
+}
